ff480bc4def404f7a676a6db081778f4f9aeca83
陳奕翔 <t111590037@ntut.org.tw>
2024-03-22 11:52:58 +0800

    ghost image imported. Fixing animation on ghost(up down right left)

--- src/AppStart.cpp
+++ src/AppStart.cpp
@@ -4,19 +4,64 @@ void App::Start() {
     LOG_TRACE("Start");
     backgroundImage = std::make_shared<BackgroundImage>("/image/Background.png");
     
-    std::vector<std::string> pacmanImage;
+    std::vector<std::string> pacmanImage, cyanImage, orangeImage, pinkImage, redImage;
     pacmanImage.reserve(3);
+    cyanImage.reserve(2);
+    orangeImage.reserve(2);
+    pinkImage.reserve(2);
+    redImage.reserve(2);
     for (int i = 0; i < 3; ++i) {
         pacmanImage.emplace_back(RESOURCE_DIR"/image/Pac/Sprite (" + std::to_string(i + 1) + ").png");
     }
 
+    for (int i = 0; i < 2; i++) {
+        cyanImage.emplace_back(RESOURCE_DIR"/image/Cyan/Sprite (" + std::to_string(i + 85) + ").png");
+        orangeImage.emplace_back(RESOURCE_DIR"/image/Orange/Sprite (" + std::to_string(i + 99) + ").png");
+        pinkImage.emplace_back(RESOURCE_DIR"/image/Pink/Sprite (" + std::to_string(i + 71) + ").png");
+        redImage.emplace_back(RESOURCE_DIR"/image/Red/Sprite (" + std::to_string(i + 57) + ").png");
+    }
+
+
+
     m_Pacman = std::make_shared<AnimatedCharacter>(pacmanImage);
-    m_Pacman->SetZIndex(5);
+    m_Pacman->SetZIndex(59);
     m_Pacman->SetVisible(true);
     m_Pacman->SetLooping(true);
     m_Pacman->SetPlaying(true);
     m_Pacman->FaceEast();
 
+    m_Cyan = std::make_shared<AnimatedCharacter>(cyanImage);
+    m_Cyan->SetZIndex(51);
+    m_Cyan->SetVisible(true);
+    m_Cyan->SetLooping(true);
+    m_Cyan->SetPlaying(true);
+    m_Cyan->SetPosition({0, 10});
+
+    m_Orange = std::make_shared<AnimatedCharacter>(orangeImage);
+    m_Orange->SetZIndex(52);
+    m_Orange->SetVisible(true);
+    m_Orange->SetLooping(true);
+    m_Orange->SetPlaying(true);
+    m_Orange->SetPosition({0, 30});
+
+    m_Pink = std::make_shared<AnimatedCharacter>(pinkImage);
+    m_Pink->SetZIndex(53);
+    m_Pink->SetVisible(true);
+    m_Pink->SetLooping(true);
+    m_Pink->SetPlaying(true);
+    m_Pink->SetPosition({0, 50});
+
+    m_Red = std::make_shared<AnimatedCharacter>(redImage);
+    m_Red->SetZIndex(54);
+    m_Red->SetVisible(true);
+    m_Red->SetLooping(true);
+    m_Red->SetPlaying(true);
+    m_Red->SetPosition({0, 70});
+
+    m_Root.AddChild(m_Cyan);
+    m_Root.AddChild(m_Orange);
+    m_Root.AddChild(m_Pink);
+    m_Root.AddChild(m_Red);
     m_Root.AddChild(m_Pacman);  
 	m_Root.AddChild(backgroundImage);
 	m_Root.Update();
--- src/AppUpdate.cpp
+++ src/AppUpdate.cpp
@@ -1,31 +1,26 @@
 #include "App.hpp"
-#include "Util/Input.hpp"
-
-float SpeedX, SpeedY, SpeedMultiplier = 1;
 
 void App::Update() {
-    
-    if (Util::Input::IsKeyDown(Util::Keycode::W)) {
-        SpeedX = 0;
-        SpeedY = 5;
-    }
-    else if (Util::Input::IsKeyDown(Util::Keycode::A)) {
-        SpeedX = -5;
-        SpeedY = 0;
-    }
-    else if (Util::Input::IsKeyDown(Util::Keycode::S)) {
-        SpeedX = 0;
-        SpeedY = -5;
-    }
-    else if (Util::Input::IsKeyDown(Util::Keycode::D)) {
-        SpeedX = 5;
-        SpeedY = 0;
-    }
-    m_Pacman->Move({SpeedX, SpeedY}, SpeedMultiplier);
 
-	m_Root.Update();
-    if (Util::Input::IsKeyUp(Util::Keycode::ESCAPE) ||
-        Util::Input::IfExit()) {
-        m_CurrentState = State::END;
-    }
+  if (Util::Input::IsKeyDown(Util::Keycode::W)) {
+    m_Pacman->Speed = {0, 5};
+    m_Pacman->SpeedMultiplier = 1;
+  } else if (Util::Input::IsKeyDown(Util::Keycode::A)) {
+    m_Pacman->Speed = {-5, 0};
+    m_Pacman->SpeedMultiplier = 1;
+  } else if (Util::Input::IsKeyDown(Util::Keycode::S)) {
+    m_Pacman->Speed = {0, -5};
+    m_Pacman->SpeedMultiplier = 1;
+  } else if (Util::Input::IsKeyDown(Util::Keycode::D)) {
+    m_Pacman->Speed = {5, 0};
+    m_Pacman->SpeedMultiplier = 1;
+  }
+
+  m_Pacman->Move(m_Pacman->Speed, m_Pacman->SpeedMultiplier);
+
+  m_Root.Update();
+  if (Util::Input::IsKeyUp(Util::Keycode::ESCAPE) || Util::Input::IfExit()) {
+    m_CurrentState = State::END;
+  }
 }
+
