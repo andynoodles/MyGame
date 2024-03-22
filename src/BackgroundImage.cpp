edef2d3a28ae21030abf61948121c87e370ce837
111590045 <t111590045@ntut.org.tw>
2024-03-21 20:43:24 -0700

    Add detect type of pixel function in BackgroundImage.

--- src/BackgroundImage.cpp
+++ src/BackgroundImage.cpp
@@ -1,10 +1,16 @@
+#include<cassert>
+
 #include "BackgroundImage.hpp"
+#include "Util/Logger.hpp"
 
 BackgroundImage::BackgroundImage(const std::string imagePath) : GameObject(std::make_unique<Util::Image>(RESOURCE_DIR+imagePath), -10) { 
 	m_Transform.scale = {2,2};
+
+	assert(GetScaledSize().x/NumberOfTilesX == GetScaledSize().y/NumberOfTilesY);
+	pixelPerTile = GetScaledSize().x/NumberOfTilesX;
 }
 
-int BackgroundImage::pixelPerTile(){
-	if(this->GetScaledSize().x/this->NumberOfTilesX != this->GetScaledSize().y/this->NumberOfTilesY)	return -1;
-	return this->GetScaledSize().x/this->NumberOfTilesX;
+const unsigned char BackgroundImage::typeOfPixel(float pixelX ,float pixelY){
+	int newX = pixelX+GetScaledSize().x/2 ,newY = GetScaledSize().y/2 - pixelY;
+	return mapDesign[newY/pixelPerTile][newX/pixelPerTile];	
 }
