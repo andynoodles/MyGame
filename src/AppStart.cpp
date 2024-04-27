dabf735394fc4e237cb2697ca9eca391031120f5
111590045 <t111590045@ntut.org.tw>
2024-04-27 22:57:03 +0800

    Fix start time bug.

--- src/AppStart.cpp
+++ src/AppStart.cpp
@@ -1,17 +1,18 @@
 #include "App.hpp"
 
 void App::Start() {
+	static unsigned long m = m_Time.GetElapsedTimeMs();
 	m_BGM.PlayStart();
 	m_BackgroundImage->Draw();
 	m_ReadyText->Draw();
-	if(m_Time.GetElapsedTimeMs() > GAME_OPENING_TIME_DURATION/2.5){
+	if(m_Time.GetElapsedTimeMs()- m > GAME_OPENING_TIME_DURATION/2.5){
 		m_Red->Draw();
 		m_Pink->Draw();
 		m_Orange->Draw();
 		m_Cyan->Draw();
 		m_Pacman->Draw();
 	}
-	if(m_Time.GetElapsedTimeMs() > GAME_OPENING_TIME_DURATION){
+	if(m_Time.GetElapsedTimeMs()-m > GAME_OPENING_TIME_DURATION + 10){
 		m_ReadyText->SetVisible(false);
 		m_ReadyText->Draw();
 
