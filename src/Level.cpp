8b377f9b48ece6a898c8cfce48f98929971b19f9
111590045 <t111590045@ntut.org.tw>
2024-04-30 14:44:21 +0800

    Add some Getter in Level class.

--- src/Level.cpp
+++ src/Level.cpp
@@ -1,8 +1,14 @@
 #include "Level.hpp"
 
 Level::Level(){
+	frightTime = {6 ,5};
+
 	pacmanSpeedMul = {0.8 ,0.9};
+	pacmanFrightSpeedMul = {0.9 ,0.95};
+
 	ghostSpeedMul = {0.75 ,0.85};
+	ghostFrightSpeedMul = {0.5 ,0.55};
+	ghostTunnelSpeedMul = {0.4 ,0.45};
 }
 
 void Level::SetLevel(unsigned int level){
@@ -13,10 +19,30 @@ void Level::AddLevel(unsigned int delta){
     CurrentLevel += delta;
 }
 
+unsigned int Level::GetCurrentLevel(){
+	return CurrentLevel;
+}
+
+int Level::GetFrightTime(){
+	return frightTime[CurrentLevel];
+}
+
 float Level::GetPacmanSpeedMul(){
 	return pacmanSpeedMul[CurrentLevel];
 }
 
+float Level::GetPacmanFrightSpeedMul(){
+	return pacmanFrightSpeedMul[CurrentLevel];
+}
+
 float Level::GetGhostSpeedMul(){
 	return ghostSpeedMul[CurrentLevel];
+}
+
+float Level::GetGhostFrightSpeedMul(){
+	return ghostFrightSpeedMul[CurrentLevel];
+}
+
+float Level::GetGhostTunnelSpeedMul(){
+	return ghostTunnelSpeedMul[CurrentLevel];
 }
