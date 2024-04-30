b4613e1fa80066ac9fb180c45d88402a84c0151d
111590045 <t111590045@ntut.org.tw>
2024-04-30 14:09:19 +0800

    Add some very basic Level's feature.

--- include/Level.hpp
+++ include/Level.hpp
@@ -1,8 +1,16 @@
+#include<vector>
+
 class Level{
 public:
+	Level();
     void SetLevel(unsigned int level);
     void AddLevel(unsigned int delta);
 
+	//Getter
+	float GetPacmanSpeedMul();
+	float GetGhostSpeedMul();
+
 private:
     unsigned int CurrentLevel;
+	std::vector<float> pacmanSpeedMul ,ghostSpeedMul;
 };
--- src/Level.cpp
+++ src/Level.cpp
@@ -1,8 +1,22 @@
 #include "Level.hpp"
 
+Level::Level(){
+	pacmanSpeedMul = {0.8 ,0.9};
+	ghostSpeedMul = {0.75 ,0.85};
+}
+
 void Level::SetLevel(unsigned int level){
     CurrentLevel = level;
 }
+
 void Level::AddLevel(unsigned int delta){
     CurrentLevel += delta;
+}
+
+float Level::GetPacmanSpeedMul(){
+	return pacmanSpeedMul[CurrentLevel];
+}
+
+float Level::GetGhostSpeedMul(){
+	return ghostSpeedMul[CurrentLevel];
 }
